7618c846e861824c5c9d847335090fd6d1959dba
Faizun-Faria <34593750+Faizun-Faria@users.noreply.github.com>
2020-06-20 19:55:57 +0600

    Add files via upload

---
+++ ALL/STL/next_permutation.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, a, b, co=0;
+    cin>>n>>a>>b;
+    string s = "aaaabb";
+    do{
+        for(int i=0; i<n; i++){
+
+        }
+
+    }while(next_permutation(s.begin(), s.end()));
+    return 0;
+}
