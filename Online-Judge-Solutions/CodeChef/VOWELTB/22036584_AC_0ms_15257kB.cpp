9c7da4f4f736ef8996044a97ab3d2bba7e378680
unknown <faizunfaria0@gmail.com>
2021-04-05 21:37:03 +0600

    deleted oj solutions

--- Online-Judge-Solutions/CodeChef/VOWELTB/22036584_AC_0ms_15257kB.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-char c;
-cin>>c;
-if((c=='A')||(c=='E')||(c=='I')||(c=='O')||(c=='U')) cout<<"Vowel";
-else cout<< "Consonant";
-return 0;
-}
