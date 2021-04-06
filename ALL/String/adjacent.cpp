14075c63b246a1cfac294d3b4e3a693f291c5dc9
Faizun-Faria <34593750+Faizun-Faria@users.noreply.github.com>
2021-04-06 14:06:17 +0600

    Add files via upload

---
+++ ALL/String/adjacent.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+AABCCC
+cnt = 4
+
+*/
+int printAdjacent(){
+    string str;
+    cin>>str;
+    int cnt = 0;
+    for(int i = 0; i < str.length() -1; i++){
+        if(str[i] == str[i+1]) cnt++;
+        else cout << str[i] ;
+    }
+    cout<<str[str.length() - 1];
+    cout<<" "<<cnt<<"\n";
+}
+
+int main(){
+    freopen("input.txt", "r", stdin);
+    int testCase;
+    cin >> testCase;
+    for(int i =0; i < testCase; i++){
+        printAdjacent();
+    }
+}
---
+++ ALL/String/capitalizeName.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+faizun faria ---> Faizun Faria
+first letter
+letter after space
+
+
+*/
+
+
+bool isUpper(char c){
+    bool flag = false;
+    if(c >= 'A' && c <= 'Z') flag = true;
+    return flag;
+}
+
+
+bool isLower(char c){
+    bool flag = false;
+    if(c >= 'a' && c <= 'z') flag = true;
+    return flag;
+}
+
+string capitalize(string line){
+    if(isLower(line[0])) line[0] -= ('a'- 'A');
+    for(int i = 1; i < line.length(); i++){
+        if(line[i-1] == ' ')    line[i] -= ('a'- 'A');
+
+        else line[i] = tolower(line[i]);
+    }
+    return line;
+}
+
+
+int main(){
+    freopen("input.txt", "r", stdin);
+    string line;
+    getline(cin,line);
+
+    line = capitalize(line);
+    cout<<line;
+}
